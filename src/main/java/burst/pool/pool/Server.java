9eee501270a7f417e427fd138de0df6a28d28f0a
Harry Phillips <harryjamesphillips@gmail.com>
2019-02-01 21:47:16 +0000

    Pool API

--- src/main/java/burst/pool/pool/Server.java
+++ src/main/java/burst/pool/pool/Server.java
@@ -2,45 +2,49 @@ package burst.pool.pool;
 
 import burst.kit.entity.BurstAddress;
 import burst.kit.util.BurstKitUtils;
+import burst.pool.miners.IMiner;
 import burst.pool.storage.config.PropertyService;
 import burst.pool.storage.config.Props;
 import burst.pool.storage.persistent.StorageService;
 import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 import fi.iki.elonen.NanoHTTPD;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.StringWriter;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 
 public class Server extends NanoHTTPD {
     private final StorageService storageService;
+    private final PropertyService propertyService;
     private final Pool pool;
     private final Gson gson = BurstKitUtils.buildGson().create();
 
     public Server(StorageService storageService, PropertyService propertyService, Pool pool) {
         super(propertyService.getInt(Props.serverPort));
         this.storageService = storageService;
+        this.propertyService = propertyService;
         this.pool = pool;
     }
 
     @Override
     public Response serve(IHTTPSession session) {
         try {
-            StringBuilder response = new StringBuilder();
             Map<String, String> params = queryToMap(session.getQueryParameterString());
             session.parseBody(new HashMap<>());
             params.putAll(session.getParms());
             if (session.getUri().startsWith("/burst")) {
-                response.append(handleBurstApiCall(session, params));
+                return NanoHTTPD.newFixedLengthResponse(Response.Status.OK, "application/json", handleBurstApiCall(session, params));
             } else if (session.getUri().startsWith("/api")) {
-                response.append(handleApiCall(session, params));
+                return NanoHTTPD.newFixedLengthResponse(Response.Status.OK, "application/json", handleApiCall(session, params));
             } else {
                 return handleCall(session, params);
             }
-            return NanoHTTPD.newFixedLengthResponse(response.toString());
         } catch (Throwable t) {
             t.printStackTrace();
             return NanoHTTPD.newFixedLengthResponse(t.getMessage());
@@ -70,7 +74,35 @@ public class Server extends NanoHTTPD {
     }
 
     private String handleApiCall(IHTTPSession session, Map<String, String> params) {
-        return "404 not found";
+        if (session.getUri().startsWith("/api/getMiners")) {
+            JsonArray jsonArray = new JsonArray();
+            List<IMiner> miners = storageService.getMiners();
+            miners.forEach(miner -> {
+                JsonObject minerJson = new JsonObject();
+                minerJson.addProperty("address", miner.getAddress().getID());
+                minerJson.addProperty("pending", miner.getPending().toUnformattedString());
+                minerJson.addProperty("estimatedCapacity", miner.getCapacity());
+                minerJson.addProperty("nConf", miner.getNConf());
+                minerJson.addProperty("share", miner.getShare());
+                jsonArray.add(minerJson);
+            });
+            return jsonArray.toString();
+        } else if (session.getUri().startsWith("/api/getConfig")) {
+            JsonObject response = new JsonObject();
+            response.addProperty(Props.nAvg.getName(), propertyService.getInt(Props.nAvg));
+            response.addProperty(Props.nMin.getName(), propertyService.getInt(Props.nMin));
+            response.addProperty(Props.maxDeadline.getName(), propertyService.getLong(Props.maxDeadline));
+            response.addProperty(Props.processLag.getName(), propertyService.getInt(Props.processLag));
+            response.addProperty(Props.feeRecipient.getName(), propertyService.getBurstAddress(Props.feeRecipient).getID());
+            response.addProperty(Props.poolFeePercentage.getName(), propertyService.getFloat(Props.poolFeePercentage));
+            response.addProperty(Props.winnerRewardPercentage.getName(), propertyService.getFloat(Props.winnerRewardPercentage));
+            response.addProperty(Props.minimumPayout.getName(), propertyService.getFloat(Props.minimumPayout));
+            response.addProperty(Props.minPayoutsPerTransaction.getName(), propertyService.getInt(Props.minPayoutsPerTransaction));
+            response.addProperty(Props.transactionFee.getName(), propertyService.getFloat(Props.transactionFee));
+            return response.toString();
+        } else {
+            return "404 not found";
+        }
     }
 
     private Response handleCall(IHTTPSession session, Map<String, String> params) throws IOException {
