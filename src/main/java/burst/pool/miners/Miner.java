8da285d6252cdc0c690835fb42f5740abdf7da69
Harry Phillips <harryjamesphillips@gmail.com>
2019-02-17 15:19:36 +0000

    Don't have lowerbound

--- src/main/java/burst/pool/miners/Miner.java
+++ src/main/java/burst/pool/miners/Miner.java
@@ -65,10 +65,9 @@ public class Miner implements Payable {
         double q1 = getMedian(data1);
         double q3 = getMedian(data2);
         double iqr = q3 - q1;
-        double lowerFence = q1 - 1.5 * iqr;
         double upperFence = q3 + 1.5 * iqr;
         for (int i = 0; i < input.size(); i++) {
-            if (input.get(i).getDeadline().longValue() < lowerFence || input.get(i).getDeadline().longValue() > upperFence)
+            if (input.get(i).getDeadline().longValue() > upperFence)
                 output.add(input.get(i).getHeight());
         }
         return output;
